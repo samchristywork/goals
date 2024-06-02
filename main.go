afa642b0ded8ffc9805e129d83b23cd1fb0cfb01
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-06-02 12:31:57 -0500

    feat: Implement API
    This is the API that the client will use to interact with the database.
    The `/goals` endpoint returns a list of records, the other ones are
    self-explanatory.

--- main.go
+++ main.go
@@ -49,6 +49,93 @@ func main() {
 		return
 	}
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/index.html")
+	})
+
+	http.HandleFunc("/goals", func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT name, startTimestamp, startAmount, endTimestamp, endAmount, currentAmount FROM goals")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer rows.Close()
+
+		var goals []Goal
+		for rows.Next() {
+			var goal Goal
+			rows.Scan(&goal.Name, &goal.StartTimestamp, &goal.StartAmount, &goal.EndTimestamp, &goal.EndAmount, &goal.CurrentAmount)
+			goals = append(goals, goal)
+		}
+
+		goalsJSON, err := json.Marshal(goals)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(goalsJSON)
+	})
+
+	http.HandleFunc("/add-goal", func(w http.ResponseWriter, r *http.Request) {
+		var goal Goal
+		err := json.NewDecoder(r.Body).Decode(&goal)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		_, err = db.Exec("INSERT INTO goals (name, startTimestamp, startAmount, endTimestamp, endAmount, currentAmount) VALUES (?, ?, ?, ?, ?, ?)", goal.Name, goal.StartTimestamp, goal.StartAmount, goal.EndTimestamp, goal.EndAmount, goal.StartAmount)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		log.Printf("Added %s", goal.Name)
+
+		w.WriteHeader(http.StatusCreated)
+	})
+
+	http.HandleFunc("/update-field", func(w http.ResponseWriter, r *http.Request) {
+		var field UpdateField
+		err := json.NewDecoder(r.Body).Decode(&field)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		sql := fmt.Sprintf("UPDATE goals SET %s = ? WHERE name = ?", field.Column)
+		_, err = db.Exec(sql, field.Value, field.Name)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		log.Printf("Updated %s in %s to %s", field.Column, field.Name, field.Value)
+
+		w.WriteHeader(http.StatusNoContent)
+	})
+
+	http.HandleFunc("/delete-goal", func(w http.ResponseWriter, r *http.Request) {
+		var goal Goal
+		err := json.NewDecoder(r.Body).Decode(&goal)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		_, err = db.Exec("DELETE FROM goals WHERE name = ?", goal.Name)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		log.Printf("Deleted %s", goal.Name)
+
+		w.WriteHeader(http.StatusNoContent)
+	})
+
 	fmt.Println("Server is running on port 8000")
 	http.ListenAndServe("0.0.0.0:8000", nil)
 }
